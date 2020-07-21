b4f75656e32ab8ccb1128ca5a385de5cfb7827db
TarasIvashko <IvashkoT@gmail.com>
2020-07-21 16:29:27 +0300

    without separating to modules(clean)

--- src/app/components/App/app.component.ts
+++ src/app/components/App/app.component.ts
@@ -10,15 +10,10 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
 
-  movies: Movie[];
 
-  constructor(private httpClient: HttpClient, private router: Router) {
-    // this.httpClient.get('https://api.themoviedb.org/3/movie/top_rated?api_key=aad0303caca77fd4942b88b9502101e3&language=en-US&page=1')
-    //   .subscribe(value => console.log(this.movies = value.results));
-    // console.log(this.movies);
+  constructor() {
   }
 
-  ////https://api.themoviedb.org/3/movie/top_rated?api_key=aad0303caca77fd4942b88b9502101e3&language=en-US&page=1
 
 }
 
